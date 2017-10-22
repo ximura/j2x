5e85dfce71e770ce5d6f0b0c0c06b124b14e6b6e
Ximura <orukin@gmail.com>
2017-10-22 23:27:06 +0300

    update algorithm

--- src/J2XTransformer.cpp
+++ src/J2XTransformer.cpp
@@ -5,7 +5,7 @@
 
 int isInvalid(char c)
 {
-  return c == '[' || c == ']' || c == '{';
+  return c == '[' || c == '{';
 }
 
 int isInvalidXmlNodeName(char c)
@@ -21,32 +21,82 @@ const char* J2XTransformer::transform(const char* pJson)
   size_t len = json.length();
 
   size_t startIndex = 0;
+  size_t endIndex = 0;
+  bool foundValue = false;
+
+  bool wroteXmlValue = false;
 
   for (size_t i = 0; i < len; ++i)
   {
     const char& c = json[i];
 
-    if (c == ':')
+    if (c == '"')
     {
-      createXmlNode(json.c_str(), startIndex, i);
+      foundValue = !foundValue;
+      if (foundValue)
+      {
+        startIndex = i;
+      }
+      else
+      {
+        endIndex = i;
+      }
+    }
+    else if (!foundValue && c == ':')
+    {
+      createXmlNode(json.c_str(), startIndex, endIndex);
       startIndex = i + 1;
+      wroteXmlValue = false;
     }
-    else if (c == '}')
+    else if (!foundValue && c == '}')
     {
-      createXmlValue(json.c_str(), startIndex, i);
-      startIndex = i + 1;
-      closeXmlNode();
-    }
-    else if (c == ',' && json[i - 1] != '}')
-    {
-      createXmlValue(json.c_str(), startIndex, i);
-      closeXmlNode();
-    }
+      if (!wroteXmlValue)
+      {
+        if (endIndex < startIndex)
+        {
+          endIndex = i;
+        }
 
-    if (c == ',')
-    {
-      startIndex = i + 1;
+        createXmlValue(json.c_str(), startIndex, endIndex);
+        wroteXmlValue = true;
+      }
+
+      closeXmlNode();
     }
+    else if (!foundValue && c == ']')
+    {
+      if (!wroteXmlValue)
+      {
+        if (endIndex < startIndex)
+        {
+          endIndex = i;
+        }
+
+        createXmlValue(json.c_str(), startIndex, endIndex);
+        wroteXmlValue = true;
+      }
+
+      closeXmlNode();
+    }
+    else if (!foundValue && c == ',')
+    {
+      if (!wroteXmlValue)
+      {
+        if (endIndex < startIndex)
+        {
+          endIndex = i;
+        }
+
+        createXmlValue(json.c_str(), startIndex, endIndex);
+        wroteXmlValue = true;
+        closeXmlNode();
+      }
+    }
+  }
+
+  while (!m_xmlTags.empty())
+  {
+    closeXmlNode();
   }
 
   return m_xml.c_str();
@@ -54,15 +104,15 @@ const char* J2XTransformer::transform(const char* pJson)
 
 void J2XTransformer::createXmlNode(const char* json, size_t startIndex, size_t endIndex)
 {
-  std::string xmlTag(&json[startIndex], &json[endIndex]);
-  xmlTag.erase(std::remove_if(xmlTag.begin(), xmlTag.end(), isInvalidXmlNodeName), xmlTag.end());
+  std::string xmlTag(&json[startIndex] + 1, &json[endIndex]);
   m_xmlTags.push(xmlTag);
   m_xml.append("<").append(xmlTag.c_str()).append(">");
 }
 
 void J2XTransformer::createXmlValue(const char* json, size_t startIndex, size_t endIndex)
 {
-  std::string xmlValue(&json[startIndex], &json[endIndex]);
+  std::string xmlValue(&json[startIndex] + 1, &json[endIndex]);
+
   m_xml.append(xmlValue);
 }
 
